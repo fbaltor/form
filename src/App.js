4a27a0c8f0c0e29e197dac88b16b5c6ebeaf0670
Felipe Baltor <fbaltor@gmail.com>
2021-09-11 14:26:05 -0300

    feat: added 'submitting' state and button disable behavior

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+    setTimeout(() => {
+      alert('form submitted');
+      setSubmitting(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple form</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button disabled={submitting} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
