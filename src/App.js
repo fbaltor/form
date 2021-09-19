ae1498f9c1a78cbbec55eb83a2a1cfcc491307ec
Felipe Baltor <fbaltor@gmail.com>
2021-09-18 23:28:55 -0300

    feat: better componentization

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import { useState } from 'react';
+import { Form } from './Form';
 import './App.css';
 
 function App() {
-  const [formData, setFormData] = useState([]);
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    setSubmitting(true);
-
-    const input = event.target[0];
-    const data = [...formData, input.value];
-    setFormData(data);
-
-    setTimeout(() => {
-      setSubmitting(false);
-    }, 2000);
-
-    input.value = '';
-  };
-
   return (
-    <div className="App">
-      <h1>todo's</h1>
-      {!!formData.length && (
-        <div>
-          <ul>
-            {formData.map((element, index) => (
-              <li key={index}>{element}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input type="text" />
-        </label>
-        <br />
-        <button disabled={submitting}>submit</button>
-      </form>
-    </div>
+    <>
+      <Form />
+    </>
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+export const Form = () => {
+  const [formData, setFormData] = useState([]);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setSubmitting(true);
+
+    const input = event.target[0];
+    const data = [...formData, input.value];
+    setFormData(data);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 2000);
+
+    input.value = '';
+  };
+
+  return (
+    <div className="App">
+      <h1>todo's</h1>
+      {!!formData.length && (
+        <div>
+          <ul>
+            {formData.map((element, index) => (
+              <li key={index}>{element}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" />
+        </label>
+        <br />
+        <button disabled={submitting}>submit</button>
+      </form>
+    </div>
+  );
+};
