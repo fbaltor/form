e3dc3af1ff2b7dc90e60c1472d28380825ad957d
Felipe Baltor <fbaltor@gmail.com>
2021-09-18 21:05:54 -0300

    feat: controlled form implementation

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import { useState, useReducer } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-const formReducer = (state, event) => {
-  return [...state, event.target['todoList'].value];
-};
-
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, []);
+  const [formData, setFormData] = useState([]);
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setFormData(event);
+
     setSubmitting(true);
+
+    const entry = event.target[0].value;
+    const data = [...formData, entry];
+    setFormData(data);
+
     setTimeout(() => {
       setSubmitting(false);
     }, 2000);
+
+    event.target[0].value = '';
   };
 
   return (
     <div className="App">
       <h1>todo's</h1>
-      {formData && (
-        <ul>
-          {formData.map((element, index) => (
-            <li key={index}>{element.toString()}</li>
-          ))}
-        </ul>
+      {!!formData.length && (
+        <div>
+          <p>list</p>
+          <ul>
+            {formData.map((element, index) => (
+              <li key={index}>{element}</li>
+            ))}
+          </ul>
+        </div>
       )}
       <form onSubmit={handleSubmit}>
-        <fieldset>
-          <label>
-            <p>todo</p>
-            <input name="todoList" />
-          </label>
-        </fieldset>
-        <button disabled={submitting} type="submit">
-          submit
-        </button>
+        <label>
+          <input type="text" />
+        </label>
+        <br />
+        <button disabled={submitting}>submit</button>
       </form>
     </div>
   );
